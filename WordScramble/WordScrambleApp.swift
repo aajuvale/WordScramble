9d7be41f46580ca958dcbd09f66cd9d2fc1eba81
aajuvale <ahmed.juvale@gmail.com>
2025-07-24 12:14:03 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ahmed Juvale on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
