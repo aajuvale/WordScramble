d1fb47f86221f72f7932367072c045a6eb1663d7
aajuvale <ahmed.juvale@gmail.com>
2025-07-25 08:54:18 -0700

    chore: implemented spell checker

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -10,15 +10,27 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-
+        
     }
 
-    func testBundles() {
-        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL){
-                // file was loaded properly
-            }
-        }
+    func testStrings() {
+//        let input = """
+//        a
+//        b
+//        c
+//        """
+//        let letters = input.components(separatedBy: "\n")
+//        let letter = letters.randomElement()
+//        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+
+        let word = "swift"
+        let checker = UITextChecker()
+
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        let allGood = misspelledRange.location == NSNotFound
+
     }
 }
 
